795eba187bd5add8f91620b324501eef98f8bed3
Cristian Suárez <cristiansuarezg7@gmail.com>
2020-03-20 19:58:09 +0100

    Contact page items added

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -9,12 +9,15 @@ import ProfilePic from '../../../assets/static/ProfilePic.png'
 import CibusPic from '../../../assets/static/CibusPic.png'
 import { Link } from 'react-router-dom';
 import ScrollIcon from '../../ScrollIcon';
+import EmailIcon from '../../EmailIcon';
+import PhoneIcon from '../../PhoneIcon';
 
 
 const HomePage = () => {
     const refContainer = useRef(0);
-
     const scrollInfo = useIndicator(1);
+    const email = "cristiansuarezg7@gmail.com";
+    const number = "+34 645 40 31 64";
 
     return (
         <div className="page" onScroll={ scrollInfo.onScroll }>
@@ -31,13 +34,15 @@ const HomePage = () => {
                     < HomeSVG />
                     <img className="profile-pic" src={ProfilePic} alt="My profile pic :)"/>
                 </div>
-                <div className="info">
-                    <h2 className="hello">HELLO!</h2>
-                    <h1 className="title">I'M CRISTIAN</h1>
-                    <span>
-                        UX/UI Designer &#38; Frontend Developer based in Seville who always wear a smile at work. 
-                    </span><br/>
-                    < Link to="/profile" className="info-btn profile-btn">Know me</Link>
+                <div className="info-container">
+                    <div className="info">
+                        <h2 className="hello">HELLO!</h2>
+                        <h1 className="title">I'M CRISTIAN</h1>
+                        <span>
+                            UX/UI Designer &#38; Frontend Developer based in Seville who always wear a smile at work. 
+                        </span><br/>
+                        < Link to="/profile" className="info-btn profile-btn">Know me</Link>
+                    </div>
                 </div>
             </section>
 
@@ -45,21 +50,30 @@ const HomePage = () => {
                 <div className="home-img">
                     <img className="projects-pic" src={CibusPic} alt="My profile pic :)"/>
                 </div>
-                <div className="info">
-                    <h1 className="title">CIBUS</h1>
-                    <span>
-                        The Point of Sale System that you don't have to learn.
-                    </span><br/>
-                    < Link to="/projects" className="info-btn project-btn">View case</Link>
+                <div className="info-container">
+                    <div className="info">
+                        <h1 className="title">CIBUS</h1>
+                        <span>
+                            The Point of Sale System that you don't have to learn.
+                        </span><br/>
+                        < Link to="/projects" className="info-btn project-btn">View case</Link>
+                    </div>
                 </div>
             </section>
 
             <section className="container">
-            <div className="info">
-                    <h1 className="title">CIBUS</h1>
-                    <span>
-                        cristiansuarezg7@gmail.com<br/>+34 645 40 31 64
-                    </span>
+                <div className="info-container">
+                    <div className="info">
+                        <h1 className="title">CIBUS</h1>
+                        <div className="contact-container">
+                            < EmailIcon />
+                            <a className="contact" href={`mailto:${email}`} >{ email }</a><br/>
+                        </div>
+                        <div className="contact-container">
+                            < PhoneIcon />
+                            <a className="contact" href={`tel:${number}`} >{ number }</a>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
