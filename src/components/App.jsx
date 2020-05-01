191fd2568f77e02516ead1ac1ab3064440e21a51
Cristian Suárez <cristiansuarezg7@gmail.com>
2020-05-01 22:42:22 +0200

    Height fix chrome mobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,17 @@ import Cibus from './pages/Cibus';
 import '../../assets/styles/App.scss'
 
 
-const App = (props) => {
+const App = () => {
+    let vh = window.innerHeight * 0.01;
+
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      });
+
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
     return (
         <BrowserRouter>
             <Switch>
